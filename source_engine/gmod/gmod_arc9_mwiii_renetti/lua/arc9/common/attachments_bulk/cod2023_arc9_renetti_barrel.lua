2e638f8d5561a3e18a5f738adfe5f2bf766b0765
ZaihnY <57522394+ZaihnDT@users.noreply.github.com>
2023-12-07 09:32:00 +0100

    renetti addition

---
+++ source_engine/gmod/gmod_arc9_mwiii_renetti/lua/arc9/common/attachments_bulk/cod2023_arc9_renetti_barrel.lua
@@ -0,0 +1,64 @@
+local ATT = {}
+---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2023 Attachments"
+ATT.PrintName = "Mk2 Renetti Long Barrel"
+ATT.CompactName = "Mk2 Renetti Long Barrel"
+ATT.Description = [[]]
+
+ATT.Icon = Material("entities/mw23_mike93_attachments/icon_attachment_vm_jup_pi_mike93_bar_long_000.png", "mips smooth")
+
+ATT.ExcludeElements = {"cod2019_muzzle_pistols"}
+
+ATT.SortOrder = 2
+ATT.Category = "cod2023_renetti_barrel"
+ATT.ActivateElements = {"barlong"}
+
+ATT.PhysBulletMuzzleVelocityMult = 1.4
+
+ARC9.LoadAttachment(ATT, "cod2022_renetti_barlong")
+---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2023 Attachments"
+ATT.PrintName = "MLX Short Competition Barrel"
+ATT.CompactName = "MLX Short Competition Barrel"
+ATT.Description = [[]]
+
+ATT.Icon = Material("entities/mw23_mike93_attachments/icon_attachment_vm_jup_pi_mike93_bar_short_000.png", "mips smooth")
+
+ATT.ExcludeElements = {"cod2019_muzzle_pistols"}
+
+ATT.SortOrder = 1
+ATT.Category = "cod2023_renetti_barrel"
+ATT.ActivateElements = {"barshort"}
+
+ATT.DamageTypeOverride = DMG_AIRBOAT
+ATT.ArmorPiercingMult = 3.5
+
+ARC9.LoadAttachment(ATT, "cod2022_renetti_barshort")
+---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2023 Attachments"
+ATT.PrintName = "MLX Extended Heavy Barrel"
+ATT.CompactName = "MLX Extended Heavy Barrel"
+ATT.Description = [[]]
+
+ATT.Icon = Material("entities/mw23_mike93_attachments/icon_attachment_vm_jup_pi_mike93_bar_longhvy_000.png", "mips smooth")
+
+ATT.ExcludeElements = {"cod2019_muzzle_pistols"}
+
+ATT.SortOrder = 3
+ATT.Category = "cod2023_renetti_barrel"
+ATT.ActivateElements = {"barlonghvy"}
+
+ATT.RangeMaxMult = 1.3
+ATT.PhysBulletMuzzleVelocityMult = 1.8
+
+ATT.SpeedMult = 0.96
+ATT.SpeedMultSights = 0.85
+
+ARC9.LoadAttachment(ATT, "cod2022_renetti_barlonghvy")
+---------------------------------------------------------------------------------------
---
+++ source_engine/gmod/gmod_arc9_mwiii_renetti/lua/arc9/shared/sh_cod2023_sounds_renetti.lua
@@ -0,0 +1,23 @@
+------------ Pistols ------------
+--- Renetti ---
+sound.Add( {
+	name = "COD2023.Renetti.Fire",
+	channel = CHAN_WEAPON,
+	level = 140,
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"weapons/cod2023/renetti/wpn_pi_mike93_plr_fire_02.ogg",
+	}
+} )
+
+sound.Add( {
+	name = "COD2023.Renetti.Fire.S",
+	channel = CHAN_WEAPON,
+	level = 140,
+	volume = 1,
+	pitch = {100,100},
+	sound = {
+		"weapons/cod2023/renetti/wpn_pi_mike93_plr_fire_01.ogg",
+	}
+} )
